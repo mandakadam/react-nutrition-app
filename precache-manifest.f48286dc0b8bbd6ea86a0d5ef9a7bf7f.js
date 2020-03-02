9978f080107e654aaa05b4cf3465ebd17fa496f8
Manda <mandakadam@gmail.com>
2020-03-02 19:48:41 +0530

    Updates

---
+++ precache-manifest.f48286dc0b8bbd6ea86a0d5ef9a7bf7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a9aeba531ee83239f111f8e1cbecc5",
+    "url": "/react-nutrition-app/index.html"
+  },
+  {
+    "revision": "542cee342bf8b67d33d9",
+    "url": "/react-nutrition-app/static/css/main.aed6f483.chunk.css"
+  },
+  {
+    "revision": "fde7d34ca200410956ea",
+    "url": "/react-nutrition-app/static/js/2.f641524e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-nutrition-app/static/js/2.f641524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542cee342bf8b67d33d9",
+    "url": "/react-nutrition-app/static/js/main.8f6ac5d8.chunk.js"
+  },
+  {
+    "revision": "2acec89833c3adf5fd6e",
+    "url": "/react-nutrition-app/static/js/runtime-main.32d712a8.js"
+  }
+]);
